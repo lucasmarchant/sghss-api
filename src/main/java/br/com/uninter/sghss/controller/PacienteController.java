ca85ece7a5a7a335665102442885614c28149735
Lucas Marchant <lucascardosomarchant@gmail.com>
2026-03-01 14:02:56 -0300

    Ajustando configurações de ordenação

--- src/main/java/br/com/uninter/sghss/controller/PacienteController.java
+++ src/main/java/br/com/uninter/sghss/controller/PacienteController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,7 +27,7 @@ public class PacienteController {
     }
 
     @GetMapping
-    public Page<DadosListagemPaciente> listar(Pageable paginacao){
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10) Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
