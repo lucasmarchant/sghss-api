b04c11358cdc13d4337f71aabc44aedf8405905b
Lucas Marchant <lucascardosomarchant@gmail.com>
2026-03-04 08:10:44 -0300

    feat: implementa tratamento de erro 400 para falhas de validação (Bean Validation)

--- src/main/java/br/com/uninter/sghss/infra/TratadorDeErros.java
+++ src/main/java/br/com/uninter/sghss/infra/TratadorDeErros.java
@@ -2,8 +2,12 @@ package br.com.uninter.sghss.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -11,5 +15,19 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404(){
     return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao:: new).toList());
+    }
+
+    private record DadosErrosValidacao(String campo, String mensagem){
+
+        public DadosErrosValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
